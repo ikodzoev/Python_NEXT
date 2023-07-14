6ee0ded08b0200226dfc4bc145c54d410d28a2f8
ikodzoev <loamaro@live.ru>
2023-07-14 21:47:56 +0300

    Доработка задач №№ 2 и 4

--- HW_1/task_4.py
+++ HW_1/task_4.py
@@ -1,5 +1,6 @@
 """"Задача № 4. Решите квадратное уравнение 5x2-10x-400=0 последовательно
 сохраняя переменные a, b, c, d, x1 и x2."""""
+import math
 
 print("Введите коэффициенты для квадратного уравнения (ax^2 + bx + c = 0):")
 a = float(input("a = "))
@@ -9,8 +10,6 @@ c = float(input("c = "))
 disc = b ** 2 - 4 * a * c
 print("Дискриминант D = %.2f" % disc)
 if disc > 0:
-    import math
-
     x1 = (-b + math.sqrt(disc)) / (2 * a)
     x2 = (-b - math.sqrt(disc)) / (2 * a)
     print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
