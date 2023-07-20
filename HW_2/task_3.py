218900e5a97be5e830a3653249b52302f694bbb5
ikodzoev <loamaro@live.ru>
2023-07-20 19:05:21 +0300

    Оптимизация задачи № 3

--- HW_2/task_3.py
+++ HW_2/task_3.py
@@ -2,22 +2,18 @@
 Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions."""
 import fractions
 
-a: str = str(input("Введите числитель первой дроби "))
-b: str = str(input("Введите знаменатель первой дроби "))
-c: str = str(input("Введите числитель второй дроби "))
-d: str = str(input("Введите знаменатель второй дроби "))
-num1: int = int(a)
-num2: int = int(b)
-num3: int = int(c)
-num4: int = int(d)
+a = input("Введите числитель первой дроби ")
+b = input("Введите знаменатель первой дроби ")
+c = input("Введите числитель второй дроби ")
+d = input("Введите знаменатель второй дроби ")
+
+num1, num2, num3, num4 = int(a), int(b), int(c), int(d)
 
 res1 = (num1 / num2) + (num3 / num4)
 res2 = (num1 / num2) * (num3 / num4)
-print(res1)
-print(res2)
+print(f"{res1}\n{res2}")
 fraction1 = fractions.Fraction(num1, num2)
 fraction2 = fractions.Fraction(num3, num4)
 res3 = fraction1 + fraction2
 res4 = fraction1 * fraction2
-print(res3)
-print(res4)
+print(f"{res3}\n{res4}")
