d7d4b582691e92552ddf1ccebd90b2e8ca1e7cda
ikodzoev <loamaro@live.ru>
2023-08-25 17:16:39 +0500

    Домашнее задание 12-го семинара: добавлены docstrings

--- HW_12/task_1.py
+++ HW_12/task_1.py
@@ -9,6 +9,10 @@ import os
 
 
 class FullNameDescriptor:
+    """
+    Дескриптор для проверки и установки полного имени студента.
+    """
+
     def __set_name__(self, owner, name):
         self.name = name
 
@@ -16,6 +20,9 @@ class FullNameDescriptor:
         return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
+        """
+        Проверяет, что все слова в полном имени начинаются с заглавной буквы и содержат только буквы.
+        """
         if not all(x.istitle() for x in value.split()):
             raise ValueError("ФИО должны начинаться с заглавной буквы")
         if not all(x.isalpha() or x.isspace() for x in value):
@@ -24,9 +31,15 @@ class FullNameDescriptor:
 
 
 class Student:
+    """
+    Класс для представления студента.
+    """
     full_name = FullNameDescriptor()
 
     def __init__(self, full_name, subjects_file):
+        """
+        Инициализирует экземпляр класса Student с заданным полным именем и списком предметов из файла.
+        """
         self.full_name = full_name
         self.subjects = []
         self.grades = {}
@@ -40,6 +53,9 @@ class Student:
                 self.test_scores[subject] = []
 
     def add_grade(self, subject, grade):
+        """
+        Добавляет оценку по заданному предмету.
+        """
         if subject not in self.subjects:
             raise ValueError(f"Предмет {subject} недопустим")
         if not 2 <= grade <= 5:
@@ -47,6 +63,9 @@ class Student:
         self.grades[subject].append(grade)
 
     def add_test_score(self, subject, score):
+        """
+        Добавляет результат теста по заданному предмету.
+        """
         if subject not in self.subjects:
             raise ValueError(f"Предмет {subject} не допустим")
         if not 0 <= score <= 100:
@@ -54,16 +73,28 @@ class Student:
         self.test_scores[subject].append(score)
 
     def average_test_score(self, subject):
+        """
+        Возвращает средний результат теста по заданному предмету.
+        """
         scores = self.test_scores[subject]
         return sum(scores) / len(scores)
 
     def average_grade(self):
+        """
+        Возвращает среднюю оценку по всем предметам.
+        """
         all_grades = []
         for grades in self.grades.values():
             all_grades.extend(grades)
         return sum(all_grades) / len(all_grades)
 
     def save_to_csv(self, filename):
+        """
+        Сохраняет информацию о студенте в файл CSV.
+
+         Если файл существует и не пустой, то информация добавляется в конец файла.
+         В противном случае создается новый файл с заголовками столбцов.
+         """
         file_exists = os.path.exists(filename) and os.path.getsize(filename) > 0
         with open(filename, 'a', newline='') as f:
             writer = csv.writer(f)
@@ -75,6 +106,9 @@ class Student:
                 writer.writerow([self.full_name, subject, grades, test_scores])
 
     def __str__(self):
+        """
+        Возвращает строковое представление информации о студенте.
+        """
         res = [f'Студент: {self.full_name}']
         for subject in self.subjects:
             res.append(f'Предмет: {subject}')
